00937bd109ee1e0d8f66e9c577fd3e6945f1982d
Dibash Chhetri <dibash@elephantventures.com>
2017-09-23 00:50:43 -0400

    Adding some makeup

---
+++ src/side-panel-container/side-panel-container.component.js
@@ -0,0 +1,10 @@
+(function() {
+    function SidePanelContainerComponentController() {
+        
+    }
+
+    angular.module('splat').component('sidePanelContainer', {
+        templateUrl: './src/side-panel-container/side-panel-container.html',
+        controller: SidePanelContainerComponentController
+    });
+})();
