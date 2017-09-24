da7ab59794f95e3d3e8a9024c4780a1b9e1169b8
Dibash Chhetri <dibash@elephantventures.com>
2017-09-23 22:51:03 -0400

    update animation

--- src/search-panel-container/search-panel-container.component.js
+++ src/search-panel-container/search-panel-container.component.js
@@ -1,30 +1,13 @@
 (function() {
-    function SearchPanelContainerComponentController(bugOperations) {
+    function SearchPanelContainerComponentController(bugOperations, $timeout) {
         var $ctrl = this;
 
-        $ctrl.filterItems = [
-            {content: 'Smart Filter', id: 0},
-            {content: 'Description', id: 1},
-            {content: 'Feature', id: 2},
-            {content: 'Date', id: 3},
-            {content: 'Priority', id: 4},
-            {content: 'Submitted By', id: 5},
-            {content: 'Status', id: 6},
-        ];
-
-        $ctrl.filterTagConstants = {};
-
         $ctrl.model = {
-            enableSmartFiltering: false,
+            enableSmartFiltering: true,
             selectedFilterTags: []
         }
 
         $ctrl.bugList = [];
-        $ctrl.featureTypes = [];
-
-        bugOperations.getAllFeatures().then( (features) => {
-            $ctrl.featureTypes = features;
-        });
 
         bugOperations.getAllBugs().then((bugs) => {
             $ctrl.bugList = bugs;
@@ -35,28 +18,10 @@
             $ctrl.model.enableSmartFiltering = !$ctrl.model.enableSmartFiltering;
         }
 
-        $ctrl.toggleTagSelection = (tagId) => {
-            var idx = _.indexOf($ctrl.model.selectedFilterTags, tagId);
-            if(idx !== -1) {
-                $ctrl.model.selectedFilterTags.splice(idx, 1);
-            } else {
-                $ctrl.model.selectedFilterTags.push(tagId);
-            }
-        }
-
-        $ctrl.isTagSelected = (tagId) => {
-            return $ctrl.model.selectedFilterTags.indexOf(tagId) !== -1;
-        }
-
-        $ctrl.hasFilterOptions = (tagId) => {
-            return tagId === 1; //feature
-        }
-
-        $ctrl.getFilterOptions = (tagId) => {
-            if (tagId === 1) {
-                return $ctrl.featureTypes;
-            }
-        }
+        //allow template to render then change model to which causes animation to trigger
+        $timeout(()=> {
+            $ctrl.model.enableSmartFiltering = false;
+        }, 200);
     }
 
     angular.module('splat').component('searchPanelContainer', {
